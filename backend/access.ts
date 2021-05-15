ae16a7a1766167e87291eed2b13f0758289009e8
Alexandre Di Guida <alexandre.diguida@gmail.com>
2021-05-15 16:17:53 +0100

    ✨ (backend): Add user and field based permissions

--- backend/access.ts
+++ backend/access.ts
@@ -73,14 +73,14 @@ export const rules = {
     // They should only see available products (based on the status field)
     return { status: 'AVAILABLE' }
   },
-  // canManageUsers({ session }: ListAccessArgs) {
-  //   if (!isSignedIn({ session })) {
-  //     return false
-  //   }
-  //   if (permissions.canManageUsers({ session })) {
-  //     return true
-  //   }
-  //   // Otherwise they may only update themselves!
-  //   return { id: session.itemId }
-  // },
+  canManageUsers({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false
+    }
+    if (permissions.canManageUsers({ session })) {
+      return true
+    }
+    // Otherwise they may only update themselves!
+    return { id: session.itemId }
+  },
 }
